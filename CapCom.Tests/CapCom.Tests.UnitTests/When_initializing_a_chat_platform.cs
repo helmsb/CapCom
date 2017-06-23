3556b046a3b555475b94326673faa1cbb11c78c5
bhelms <blake@blakehelms.net>
2017-06-23 18:55:59 -0400

    Added unit test project

---
+++ CapCom.Tests/CapCom.Tests.UnitTests/When_initializing_a_chat_platform.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace CapCom.Tests.UnitTests
+{
+    public class When_initializing_a_chat_platform
+    {
+        [Fact]
+        public void Should_return_true_when_chat_platform_succesfully_initalizes()
+        {
+
+        }
+
+        [Fact]
+        public void Should_return_false_when_chat_platform_succesfully_initalizes()
+        {
+            
+        }
+    }
+}
